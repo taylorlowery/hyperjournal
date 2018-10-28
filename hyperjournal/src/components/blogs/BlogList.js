1c53a84639cc67aa9de9d1e0f8330dd2f5f3c075
admiraltaylor <admiral.taylor@gmail.com>
2018-10-28 14:30:46 -0700

    initial setup of dashboard, notification, and summary components

---
+++ hyperjournal/src/components/blogs/BlogList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const BlogList = () => {
+    return (
+        <div className="blog-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Blog Title</span>
+                    <p>Posted By Tater</p>
+                    <p className="grey-text">Oct. 28, 2016</p>
+                </div>
+            </div>
+            
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Blog Title</span>
+                    <p>Posted By Tater</p>
+                    <p className="grey-text">Oct. 28, 2016</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Blog Title</span>
+                    <p>Posted By Tater</p>
+                    <p className="grey-text">Oct. 28, 2016</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default BlogList;
