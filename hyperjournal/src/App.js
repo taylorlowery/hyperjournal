e60d77aa77abb2c21233d2d7ddae12a91ffc970b
admiraltaylor <admiral.taylor@gmail.com>
2018-10-28 14:17:17 -0700

    added initial navbar components

--- hyperjournal/src/App.js
+++ hyperjournal/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import Navbar from './components/layout/Navbar';
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>HyperJournal</h1>
+          <Navbar />
         </div>
       </BrowserRouter>      
     );
