54d46b98b3e11809f5ee27917d450b8c869b35d7
admiraltaylor <admiral.taylor@gmail.com>
2018-10-28 15:17:21 -0700

    created basic auth form front end

--- hyperjournal/src/App.js
+++ hyperjournal/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import DashBoard from './components/dashboard/Dashboard'
 import Post from './components/posts/Post';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component {
   render() {
@@ -14,6 +16,8 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={DashBoard} />
             <Route path='/post/:id' component={Post} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>      
