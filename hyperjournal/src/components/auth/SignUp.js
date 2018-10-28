3803ba504523c730dfeeecfa1adf0b0e7c9e68c7
admiraltaylor <admiral.taylor@gmail.com>
2018-10-28 15:46:44 -0700

    fixed padding on several forms, added background

--- hyperjournal/src/components/auth/SignUp.js
+++ hyperjournal/src/components/auth/SignUp.js
@@ -19,28 +19,32 @@ class SignUp extends Component {
   render() {
     return (
       <div className="container">
-          <form onSubmit={this.handleSubmit} className="white">
-              <h5>Sign Up</h5>
-              <div className="input-field">
-                  <label htmlFor="email">Email</label>
-                  <input type="email" name="email" id="email" onChange = {this.handleChange} />
+          <div className="card">
+              <div className="card-content">
+              <form onSubmit={this.handleSubmit} className="white">
+                    <h5>Sign Up</h5>
+                    <div className="input-field">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="email" onChange = {this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" onChange = {this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" name="firstName" id="firstName" onChange = {this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input type="text" name="lastName" id="lastName" onChange = {this.handleChange}/>
+                    </div>
+                    <div className="input-field">
+                        <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+                    </div>
+                </form>
               </div>
-              <div className="input-field">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" name="password" id="password" onChange = {this.handleChange} />
-              </div>
-              <div className="input-field">
-                  <label htmlFor="firstName">First Name</label>
-                  <input type="text" name="firstName" id="firstName" onChange = {this.handleChange}/>
-              </div>
-              <div className="input-field">
-                  <label htmlFor="lastName">Last Name</label>
-                  <input type="text" name="lastName" id="lastName" onChange = {this.handleChange}/>
-              </div>
-              <div className="input-field">
-                  <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
-              </div>
-          </form>
+          </div>          
       </div>
     )
   }
