8efffd81a34df806af3b5ba4573680d88240dd23
admiraltaylor <admiral.taylor@gmail.com>
2018-10-28 17:45:04 -0700

    added firebase API key

---
+++ src/config/fbConfig.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB8Zqa72mFVXa2Lqw9umI-QAmAUeAFlELE",
+    authDomain: "blog-backend-60264.firebaseapp.com",
+    databaseURL: "https://blog-backend-60264.firebaseio.com",
+    projectId: "blog-backend-60264",
+    storageBucket: "blog-backend-60264.appspot.com",
+    messagingSenderId: "80657895299"
+  };
+  firebase.initializeApp(config);
