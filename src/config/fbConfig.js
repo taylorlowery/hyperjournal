4607212bef93c2fca8819f21ee6dbcd7a86484d8
admiraltaylor <admiral.taylor@gmail.com>
2018-10-31 15:58:41 -0700

    fixed gitignore

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,12 +4,7 @@ import 'firebase/firestore'
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyB8dPmSytF2m-AVed-wu8PWC7AbaqG83IU",
-    authDomain: "blog-backend-a1d25.firebaseapp.com",
-    databaseURL: "https://blog-backend-a1d25.firebaseio.com",
-    projectId: "blog-backend-a1d25",
-    storageBucket: "blog-backend-a1d25.appspot.com",
-    messagingSenderId: "289235725241"
+    //add your own firebase credentials here!
   };
 
   firebase.initializeApp(config);
